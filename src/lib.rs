7ed25880f44677c4755924ffacf8fdfb68e0de70
Ben Boeckel <mathstuf@gmail.com>
2017-02-19 12:43:51 -0500

    cargo: add boilerplate

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// Distributed under the OSI-approved BSD 2-Clause License.
+// See accompanying file LICENSE for details.
+
+//! BulletML
+//!
+//! A BulletML parser and interpreter.
+
+#![warn(missing_docs)]
+
+#[macro_use]
+extern crate error_chain;
+
+mod error;
